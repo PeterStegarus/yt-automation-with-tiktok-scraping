42dba7f121cdeffd9ea46e0215e839c3e5cb63d9
PeterStgg <stegarusp@gmail.com>
2021-09-30 01:20:21 +0300

    changed puppeteer behavior when waiting for ttdownloader.com download button

--- bin/scrape-files/download-tiktok.js
+++ bin/scrape-files/download-tiktok.js
@@ -3,8 +3,10 @@ const downloadVidFromUrl = require("./download-vid-from-url.js");
 
 async function downloadTiktok(browser, video, index) {
     const page = await browser.newPage();
+    await page.setDefaultTimeout(0);
     await page.goto(video.ttdownloaderUrl);
-    await page.waitForNavigation();
+    // await page.waitForNavigation();
+    await page.waitForSelector('.download-link')
     console.log(`page loaded [${index}]`);
     // await page.screenshot({ path: 'puppeteerScreenshot.png' });
     const downloadLink = await page.$eval('.download-link', el => el.getAttribute("href"));
