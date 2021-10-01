34d1a23475d80a6d235eff6e26d4b116f4946b46
PeterStgg <stegarusp@gmail.com>
2021-10-02 00:08:55 +0300

    removing # from video titles because youtube probably flags multiple tags as spam and vids don't get recommended

--- bin/scrape-files/scrape.js
+++ bin/scrape-files/scrape.js
@@ -21,7 +21,7 @@ async function scrapeInit(category) {
 async function downloadVids(browser, category, vids, logVids, downloadPath) {
     for (const i in vids) {
         let { webVideoUrl: url, text: description } = vids[i];
-        description = description.replace(/[\/\\.'":|*?<>{}]/g, '');
+        description = description.replace(/[\/\\.'":|*?#<>{}]/g, '');
         const ttdownloaderUrl = "https://ttdownloader.com/?url=" + url;
         if (logVids.find(element => element.ttdownloaderUrl == ttdownloaderUrl))
             continue;
